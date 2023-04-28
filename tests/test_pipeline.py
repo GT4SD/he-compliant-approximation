4e3f3a07eb7b61555a478191a121ab5a9eb7375c
Alessio Bray <alessio.bray@gmail.com>
2023-04-28 11:06:33 +0200

    test: approximation pipeline initialization.

---
+++ tests/test_pipeline.py
@@ -0,0 +1,36 @@
+"""Testing the approximation pipeline."""
+
+import tempfile
+
+from hela.approximation.pipeline.core import ApproximationPipeline
+from hela.models.vanilla_transformer.configuration import (
+    VanillaTransformerConfig,
+)
+from hela.models.vanilla_transformer.model import VanillaTransformer
+from hela.pytorch_lightning.models.approximations.vanilla_transformer import (
+    LitApproximatedVanillaTransformer,
+)
+
+
+def test_init_approximation_pipeline():
+    """Tests the initialization of an approximation pipeline."""
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        steps_path = "./pipeline_steps.json"
+
+        trainer_args = {}
+        trainer_args["experiment_log_dir"] = tmpdirname
+        trainer_args["experiment_name"] = "try"
+
+        model = VanillaTransformer(VanillaTransformerConfig())
+
+        pipeline = ApproximationPipeline(
+            model=model,
+            lightning_model_class=LitApproximatedVanillaTransformer,
+            trainer_args=trainer_args,
+            pipeline_steps_path=steps_path,
+        )
+
+        # ASSERTS
+
+        assert isinstance(pipeline, ApproximationPipeline)
+        assert isinstance(pipeline.lightning_model, LitApproximatedVanillaTransformer)
