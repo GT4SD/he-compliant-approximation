c40f269407ada383f7ec253f1163a9620f3ca84f
AlessioBray <alessio.bray@gmail.com>
2024-03-14 16:13:51 +0100

    fix: LeNet greyscale argument.
    Fixed specification of greyscale input images for LeNet.

--- src/hela/pytorch_lightning/models/approximations/lenet.py
+++ src/hela/pytorch_lightning/models/approximations/lenet.py
@@ -56,6 +56,8 @@ class LitApproximatedLeNet(LitApproximatedCNN):
         # model configuration arguments
         parser.add_argument("--lenet_type", type=str, default="LeNet-5")
         parser.add_argument("--num_classes", type=int, default=10)
-        parser.add_argument("--greyscale", dest="greyscale", action="store_true", default=False)
+        parser.add_argument(
+            "--greyscale", dest="greyscale", action="store_true", default=False
+        )
 
         return parser
