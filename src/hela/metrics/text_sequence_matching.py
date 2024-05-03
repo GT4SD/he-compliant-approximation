6e2724816b5d5a668b70a33bdb494873d5985534
AlessioBray <alessio.bray@gmail.com>
2024-05-03 11:41:30 +0200

    feat: added custom SequenceMatchingMetric.

---
+++ src/hela/metrics/text_sequence_matching.py
@@ -0,0 +1,21 @@
+"""Implementation of accuracy metric based on entire text sequence matching."""
+
+import torch
+from torch import Tensor
+from torchmetrics import Metric
+
+
+class SequenceMatchingMetric(Metric):
+    """Sequence Matching Metric."""
+
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.add_state("correct", default=torch.tensor(0), dist_reduce_fx="sum")
+        self.add_state("total", default=torch.tensor(0), dist_reduce_fx="sum")
+
+    def update(self, preds: Tensor, target: Tensor) -> None:
+        self.correct += torch.equal(preds, target)
+        self.total += preds.shape[0]
+
+    def compute(self) -> None:
+        return self.correct.float() / self.total
